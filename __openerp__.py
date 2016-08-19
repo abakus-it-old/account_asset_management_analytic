767c6a54356d2e45ab4715be12fddd43293b303f
Valentin Thirion <valentinthirion@gmail.com>
2016-08-19 10:07:03 +0200

    Version number change

--- __openerp__.py
+++ __openerp__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 {
     "name": "Account Asset Analytic by AbAKUS",
-    "version": "1.0",
+    "version": "9.0.1.0",
     "author": "AbAKUS it-solutions",
     "category": "Accounting",
     "website": "http://www.abakusitsolutions.eu",
