632c6e01e566cc0f8c48d1b7d6b577538349618d
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-20 10:09:32 +0200

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 {
-    "name": "Account Asset Analytic by AbAKUS",
+    "name": "Account Asset Analytic",
     "version": "9.0.1.0",
     "author": "AbAKUS it-solutions",
     "category": "Accounting",
@@ -12,11 +12,11 @@
     ],
     "description": 
     """
-Account Asset Analytic for NOVIAT Asset module
+Account Asset Analytic for oddo 9 Asset module
 
 This module adds a field 'account analytic' on assets in order to generate depreciation an move lines that are linked to an analytic account.
 
-This module is ispired by VAUXOO's one but transformed to work with NOVIAT's Assets Management module by Valentin THIRION at AbAKUS it-solutions.
+This module is ispired by VAUXOO's one but transformed to work with odoo's 9 Assets Management module by Valentin THIRION at AbAKUS it-solutions.
     """,
     "data": [
         "view/view.xml",
